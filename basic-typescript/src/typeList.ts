a83b4e162af563179084059d34e603b69512586f
rintarotajima <rintarotajima@gmail.com>
2024-10-12 23:58:49 +0900

    TypeScirptの基本の型

---
+++ basic-typescript/src/typeList.ts
@@ -0,0 +1,37 @@
+/** TypeScriptの基本の型 
+ * : データ型　→　その値の型を指定することができる。
+*/
+
+//  boolean
+// let bool: boolean = true;
+
+//  number 数値
+// let num: number = 0;
+
+//  string 文字
+// let str: string = "A";
+
+//  Array 配列
+// let arr1: Array<number> = [0, 1, 2];
+// let arr2: number[] = [0, 1, 2];
+
+//  tuple
+// let tuple: [number, string] = [0, "A"];
+
+//  any 
+// let any1: any = false;
+
+//  void
+// const funcA = (): void => {
+//     const test = "Test"
+// }
+
+//  null
+// let null1: null = null;
+
+//  undefined
+// let undefined1: undefined = undefined;
+
+//  Object
+// let obj1: object = {};
+// let obj2: { id: number, name: string } = { id: 0, name: "Rintaro"};
