1c7509884766fc41c4e4767253f7606600bcea80
rintarotajima <rintarotajima@gmail.com>
2024-10-13 22:52:35 +0900

    設定ファイルをいじってみる

--- basic-typescript/src/App.tsx
+++ basic-typescript/src/App.tsx
@@ -1,6 +1,7 @@
 import { Practice1 } from "./practice/Practice1";
 import { Practice2 } from "./practice/Practice2";
 import { Practice3 } from "./practice/Practice3";
+import { Practice4 } from "./practice/Practice4";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Practice1 />
       <Practice2 />
       <Practice3 />
+      <Practice4 />
     </div>
   );
 }
