319070c34be60f4b8cf641cdc2470ddf1a24fe5b
rintarotajima <rintarotajima@gmail.com>
2024-10-13 22:42:11 +0900

    変数の型指定

--- basic-typescript/src/App.tsx
+++ basic-typescript/src/App.tsx
@@ -1,11 +1,13 @@
 import { Practice1 } from "./practice/Practice1";
 import { Practice2 } from "./practice/Practice2";
+import { Practice3 } from "./practice/Practice3";
 
 function App() {
   return (
     <div>
       <Practice1 />
       <Practice2 />
+      <Practice3 />
     </div>
   );
 }
