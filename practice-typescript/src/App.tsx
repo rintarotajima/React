63691f61ba9c1a763140da7ca0ed5bb1cde9759b
rintarotajima <rintarotajima@gmail.com>
2024-10-14 23:13:01 +0900

    React×TypeScriptの準備

---
+++ practice-typescript/src/App.tsx
@@ -0,0 +1,15 @@
+import axios from "axios"
+
+
+export default function App() {
+  const onClickFetchData = () => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      console.log(res);
+    })
+  }
+  return (
+    <div>
+      <button onClick={onClickFetchData}>データ取得</button>
+    </div>
+  )
+}
