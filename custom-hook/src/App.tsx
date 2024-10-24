91bbe7559cdb8c86e929ea01d80ffc06f43c5c42
rintarotajima <rintarotajima@gmail.com>
2024-10-24 22:02:15 +0900

    カスタムフックを使ったデータ取得例

--- custom-hook/src/App.tsx
+++ custom-hook/src/App.tsx
@@ -1,8 +1,5 @@
-import axios from "axios";
 import { UserCard } from "./components/UserCard";
-import { User } from "./types/api/user";
-import { useState } from "react";
-import { UserProfile } from "./types/userProfile";
+import { useAllUsers } from "./hooks/useAllUsers";
 
 // const user = {
 //   id: 1,
@@ -12,46 +9,24 @@ import { UserProfile } from "./types/userProfile";
 // };
 
 export default function App() {
-  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  const onClickFetchUser = () => {
-    setLoading(true);
-    setError(false);
-    axios
-      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        const data = res.data.map((user) => ({
-          id: user.id,
-          name: `${user.name}(${user.username})`,
-          email: user.email,
-          address: `${user.address.city}${user.address.suite}${user.address.street}`,
-        }));
-        setUserProfiles(data);
-      })
-      .catch(() => {
-        setError(true);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
-
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
+  const onClickFetchUser = () => getUsers();
   return (
     <>
       <button onClick={onClickFetchUser}>データ取得</button>
-      {error ? (
-        <p style={{ color: "red" }}>データの取得に失敗しました</p>
+      <br />
+      {error ?  (
+        <p style={{color: "red"}}>データ取得に失敗しました</p>
       ) : loading ? (
         <p>Loading...</p>
       ) : (
         <>
-          {userProfiles.map((user) => (
-            <UserCard key={user.id} user={user} />
-          ))}
+        {userProfiles.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
         </>
-      )}
+      )
+      }
     </>
   );
 }
